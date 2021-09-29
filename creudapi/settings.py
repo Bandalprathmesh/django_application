8f8abcb09da8ba9ff8fa3c2f9ca351a33ac0b8eb
Bandalprathmesh <prathmesh0399@gmail.com>
2021-09-29 11:28:43 +0530

    updated database

--- creudapi/settings.py
+++ creudapi/settings.py
@@ -83,6 +83,18 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+# 'default': {
+# 'ENGINE': 'django.db.backends.mysql',
+# 'NAME': 'interview_process',
+# 'USER': 'priya',
+# 'PASSWORD': 'root',
+# 'HOST': 'localhost', # Or an IP Address that your DB is hosted on
+# 'PORT': '3306',
+# }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
